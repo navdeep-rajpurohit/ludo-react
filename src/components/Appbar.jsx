03bca9d382c87b80a8814508c1831a9b8d7b38c6
Navdeep <navdeep.raj.bkn@gmail.com>
2023-11-11 12:18:15 +0530

    second one

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -8,7 +8,7 @@ import { useNavigate } from "react-router-dom";
 function Appbar(){
     const navigate = useNavigate();
     return <div id='appbar-parent'>
-       <div><Typography variant={"h6"}>Ludo</Typography></div>
+       <div><Typography variant={"h6"}>Play Ludo</Typography></div>
        <div id='appbar-buttons'>
         <Button variant={"outlined"} onClick={() => {navigate('/login')}}>Login</Button>
         <Button variant={"outlined"} onClick={() => {navigate('/register')}}>Register</Button>
