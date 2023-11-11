8c260cd8fb24496ffdfe636308a79730efaa235b
Navdeep <navdeep.raj.bkn@gmail.com>
2023-11-11 14:19:59 +0530

    added logo

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -8,7 +8,19 @@ import { useNavigate } from "react-router-dom";
 function Appbar(){
     const navigate = useNavigate();
     return <div id='appbar-parent'>
-       <div><Typography variant={"h6"}>Play Ludo</Typography></div>
+       <div id={'logo-main'}>
+        <div id={'logo'}>
+         <div>
+            <div id={'first'}></div>
+            <div id={'second'}></div>
+         </div>
+         <div> 
+            <div id={'third'}></div>
+            <div id={'fourth'}></div>
+         </div>
+         <Typography id={'logo-name'} variant={"h6"}> Ludo</Typography>
+        </div>
+       </div>
        <div id='appbar-buttons'>
         <Button variant={"outlined"} onClick={() => {navigate('/login')}}>Login</Button>
         <Button variant={"outlined"} onClick={() => {navigate('/register')}}>Register</Button>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -9,8 +9,10 @@ function Register(){
         <Typography variant={"h6"}>Welcome to Ludo, let's play</Typography> <br/>
         <Card variant="outlined" style={{width: 400, padding: 20}}>
         <Typography variant={"h6"}>Register here</Typography> <br/>
+        <TextField fullWidth={true} id="outlined-basic" label="Name" variant="outlined" /> <br/> <br/>
         <TextField fullWidth={true} id="outlined-basic" label="Email" variant="outlined" /> <br/> <br/>
         <TextField fullWidth={true} id="outlined-basic" label="Password" variant="outlined" /><br/><br/>
+        <TextField fullWidth={true} id="outlined-basic" label="Re-password" variant="outlined" /><br/><br/>
         <Button variant="contained">Signup</Button>
         </Card>
     </div>
